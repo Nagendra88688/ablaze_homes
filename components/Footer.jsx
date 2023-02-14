f458b363433948d6311147d014783325cc72e30d
Nagendra singh <nagendracoolsingh@gmail.com>
2023-02-15 01:03:48 +0530

    importing dependencies

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,16 @@
-const { Box } = require("@chakra-ui/react");
+// const { Box } = require("@chakra-ui/react");
+import { Box } from "@chakra-ui/react";
 
-const Footer =()=>(
-    <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.500">
-         2023 Ablaze Homes Inc.
-    </Box> 
-)
+const Footer = () => (
+  <Box
+    textAlign="center"
+    p="5"
+    color="gray.600"
+    borderTop="1px"
+    borderColor="gray.500"
+  >
+    2023 Ablaze Homes Inc.
+  </Box>
+);
 
-export default Footer
+export default Footer;
